5947ae5f0f023283fcf7266643ef52ab6e317dd1
Scott Matsuda <smatsuda2014@gmail.com>
2019-10-14 04:53:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   counter = 0
   while array[counter] do
-    puts array[counter] = array[counter] * array[counter]
+    new_numbers << array[counter] * array[counter]
     counter += 1
 end
